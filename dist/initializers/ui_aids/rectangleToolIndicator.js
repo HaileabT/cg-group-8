f7485cf1d0b71603f1ca80163ed2413068d4b3d6
HaileabT <Haileabtesfaye11@gmail.com>
2025-01-24 18:08:30 +0300

    removed the anchor indicators

--- dist/initializers/ui_aids/rectangleToolIndicator.js
+++ dist/initializers/ui_aids/rectangleToolIndicator.js
@@ -11,6 +11,10 @@ import { elementSelector } from "../../utils/elementSelector.js";
 export class RectToolIndicator {
     constructor(el) {
         this.divElement = null;
+        this.anchorX = undefined;
+        this.anchorY = undefined;
+        this.deltaX = 0;
+        this.deltaY = 0;
         this.divElement = el;
     }
     static getIndicator() {
@@ -25,14 +29,22 @@ export class RectToolIndicator {
             return RectToolIndicator.indicator;
         });
     }
+    resetAnchor() {
+        this.anchorX = undefined;
+        this.anchorY = undefined;
+    }
     hideIndicator() {
         if (!this.divElement)
             return;
+        this.resetAnchor();
         this.divElement.style.display = "none";
     }
-    showIndicator() {
+    showIndicator(x, y) {
         if (!this.divElement)
             return;
+        this.anchorX = this.anchorX ? this.anchorX : x;
+        this.anchorY = this.anchorY ? this.anchorY : y;
+        console.log("Anchor Points: ", this.anchorX, this.anchorY, x, y);
         this.divElement.style.display = "block";
     }
     moveIndicatorTo(x, y) {
@@ -41,11 +53,31 @@ export class RectToolIndicator {
         this.divElement.style.left = `${x}px`;
         this.divElement.style.top = `${y}px`;
     }
-    resizeIndicator(w, h) {
+    resizeIndicator(x, y, deltaX, deltaY, ctx) {
+        var _a, _b, _c, _d;
         if (!this.divElement)
             return;
-        this.divElement.style.width = `${w}px`;
-        this.divElement.style.height = `${h}px`;
+        const currentX = parseFloat(getComputedStyle(this.divElement).left.slice(0, -2));
+        const currentY = parseFloat(getComputedStyle(this.divElement).top.slice(0, -2));
+        const newX = Math.min(currentX, x);
+        const newY = Math.min(currentY, y);
+        console.log("This deltas: ", this.deltaX, this.deltaY);
+        console.log("New deltas: ", deltaX, deltaY);
+        const xDeltaAreOpposite = this.deltaX * deltaX < 0;
+        const yDeltaAreOpposite = this.deltaY * deltaY < 0;
+        this.anchorX = xDeltaAreOpposite && x < ((_a = this.anchorX) !== null && _a !== void 0 ? _a : x) ? x : this.anchorX;
+        this.anchorY = yDeltaAreOpposite && y < ((_b = this.anchorY) !== null && _b !== void 0 ? _b : y) ? y : this.anchorY;
+        this.divElement.style.left = `${newX}px`;
+        this.divElement.style.top = `${newY}px`;
+        this.divElement.style.width = `${Math.abs(((_c = this.anchorX) !== null && _c !== void 0 ? _c : x) - x)}px`;
+        this.divElement.style.height = `${Math.abs(((_d = this.anchorY) !== null && _d !== void 0 ? _d : y) - y)}px`;
+        this.deltaX = deltaX;
+        this.deltaY = deltaY;
+    }
+    isIndicatorVisible() {
+        if (!this.divElement)
+            return;
+        return this.divElement.style.display !== "none";
     }
 }
 RectToolIndicator.indicator = null;
